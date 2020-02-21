fcc55ee0f7920873c27a545f386784c520de22b4
NikosTsili <61095714+NikosTsili@users.noreply.github.com>
2020-02-21 20:06:51 +0200

    Update Ergasia1.py

--- Ergasia1.py
+++ Ergasia1.py
@@ -1,19 +1,25 @@
 from itertools import count 
 
 cnt = count()
+#άνοιγμα, ανάγνωση αρχείου κατά λέξεις.
 with open('data.txt', encoding='UTF-8') as infile:
 			words = infile.read().split()
+#δημιουργία λίστας με ταξινόμηση των 5 μεγαλύτερων λέξεων, από την μεγαλύτερη στην μικρότερη
 wordlist=sorted(words, key = lambda w : (len(w), next(cnt)),  
 										reverse = True)[:5] 
 print(wordlist)
 
 for i in range(5): 
+	#ανάγνωση κάθε λέξης ξεχριστά
 	word = wordlist[i]
 	n = len(word)
+	#δημιουργία κενής λίστας που θα μπει η αντεστραμένη λέξη
 	r = [' '] * n 
 	print(word)
-	for x in range(n):        
+	for x in range(n): 
+		#αντιστροφή λέξης
 		r[x] = word[n - 1 - x]
+		#απαλοιφή φωνηέντων
 		if (r[x] != 'a' and r[x] != 'e' and 
 			r[x] != 'i' and r[x] != 'o' and 
 			r[x] != 'u'): 
